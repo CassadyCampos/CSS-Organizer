68e135e3814c48f7c1b64ce3eb128119ad333ad1
Cassady <cassady801@gmail.com>
2020-05-21 13:55:11 -0600

    Started function that removes extra lines from css files

--- main.cpp
+++ main.cpp
@@ -10,21 +10,26 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <stdio.h>
+
 
 void sortFile();
-void readInStyleRules(std::fstream& fin, std::vector<std::string>& lines);
+void readInStyleRules(std::fstream& fin);
 
 std::string filePath;
+std::string tempFile = "temp.txt";
 
-enum ruleType {Id, Class};
+enum ruleType {Id = 0, Class};
 
 struct Ruling {
     ruleType type;
-    int numberOfLines;
+    // int numberOfLines;
     std::vector<std::string> lines;
-    Ruling(ruleType t, int n, std::vector<std::string> l) : type(t), numberOfLines(n),
-    lines(l) {}
+    Ruling(ruleType t, std::vector<std::string> l) : type(t) , lines(l) {}
 };
+
+std::vector<Ruling> rulings;
 
 int main() {
 
@@ -34,40 +39,57 @@ int main() {
     std::cin >> filePath;
 
     std::fstream fin;
-    std::vector<std::string> lines;
+
 
     fin.open(filePath);
 
     // readInStyleRules(fin, lines);
-    readInStyleRules(fin, lines);
+    readInStyleRules(fin);
+
+    std::cout << "After function" << std::endl;
+    for (Ruling r : rulings) { 
+        std:: cout << "Rule Type: " << r.type << std::endl;
+        std::cout << "Contents: " << std::endl;
+        for (std::string line : r.lines) {
+            std::cout << line << std::endl;
+        }
+    }
+
+    fin.close();
     
-
-    if (!fin.is_open()) {
-        std::cout << "Couldn't open file." << std::endl;
-        return 0;
-
-    };
-
-    std::cout << "File reached and opened successfully." << std::endl;
-
 
     return 0;
 }
 
-void readInStyleRules(std::fstream& fin, std::vector<std::string>& lines) {
+void readInStyleRules(std::fstream& fin) {
     //* Check if file is still opened
     if (!fin.is_open()) {
-        std::cout << "In function couldnt open" << std::endl;
+        std::cout << "Could not open file" << std::endl;
         return;
     };
 
     std::cout << "In function opened successfully" << std::endl;
     std::string line;
-    while(!fin.eof()) {
-        std::getline(fin, line);
-        std::cout << line;
-    };
+    std::vector<std::string> lines;
+    std::ofstream fout;
+    fout.open(tempFile, std::ios::out);
 
+
+    while(getline(fin, line)) {
+        
+        //* The line is blank
+        while (line.length() == 0) {
+            getline(fin, line);
+        }
+
+        //* The line is not blank
+        fout << line << std::endl;
+
+    }
+    fout.close();
+
+    std::remove(filePath.c_str());
+    std::rename(tempFile.c_str(), filePath.c_str());
 }
 
 void sortFile() {
