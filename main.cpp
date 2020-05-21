86bc92ded56af7609f65eb5723b294b149c2475d
Cassady <cassady801@gmail.com>
2020-05-21 17:07:53 -0600

    Working on file sorting of rule names

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <algorithm>
 #include <stdio.h>
+#include <sstream>
 
 
 void sortFile();
@@ -47,13 +48,7 @@ int main() {
     readInStyleRules(fin);
 
     std::cout << "After function" << std::endl;
-    for (Ruling r : rulings) { 
-        std:: cout << "Rule Type: " << r.type << std::endl;
-        std::cout << "Contents: " << std::endl;
-        for (std::string line : r.lines) {
-            std::cout << line << std::endl;
-        }
-    }
+
 
     fin.close();
     
@@ -71,23 +66,61 @@ void readInStyleRules(std::fstream& fin) {
     std::cout << "In function opened successfully" << std::endl;
     std::string line;
     std::vector<std::string> lines;
+
+    //* Create file for writing out
     std::ofstream fout;
     fout.open(tempFile, std::ios::out);
+
+    ruleType type;
 
 
     while(getline(fin, line)) {
         
-        //* The line is blank
+        //* The line is blank - remove it
         while (line.length() == 0) {
             getline(fin, line);
         }
 
-        //* The line is not blank
+        //* The line is not blank - keep it
+        std::vector<std::string> tokens;
+        std::stringstream check1(line);
+        std::string intermediate;
+
+
+        //* Tokenizing by space ' ' 
+        while (getline(check1, intermediate, ' ')) {
+            tokens.push_back(intermediate);
+        }
+
+        // //* If first token belongs to class to has an id
+        if (tokens.begin()[0] == "." || tokens.begin()[0] == "#") {
+            tokens.begin()[0] == "." ? type = ruleType::Class : 
+            type = ruleType::Id;
+        } 
+
+        // //* print out tokens
+        // for (int i = 0; i < tokens.size(); i++) {
+        //     std::cout << tokens[i] << " " << std::endl;
+        // } 
+
+        //* We are at the end of the rule here
+        if (tokens.begin()[0] == "}") {
+            lines.push_back(line);
+            Ruling rule(type, lines);
+            lines.clear();
+            // break;
+        } else {
+            lines.push_back(line);
+        }
+
+        
+        //* Print back out our line
         fout << line << std::endl;
 
     }
-    fout.close();
 
+    fout.close();
+    //* Overwrite the file with our existing changes
     std::remove(filePath.c_str());
     std::rename(tempFile.c_str(), filePath.c_str());
 }
